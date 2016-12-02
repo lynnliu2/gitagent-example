b04055304c0ac4cbd73f8d28e85bf0f3384b709a
Lynn <lin.liu2@hpe.com>
2016-12-02 17:10:06 +0800

    It is a demo

---
+++ action.cs
@@ -0,0 +1,19 @@
+//---------------------------------------------
+//Script Title        :
+//Script Description  :
+//
+//
+//Recorder Version    : 
+//---------------------------------------------
+
+namespace Script
+{
+    public partial class VuserClass
+    {
+        public int Action()
+        {
+        	lr.log_message("This is a demostration!");
+            return 0;
+        }
+    }
+}
